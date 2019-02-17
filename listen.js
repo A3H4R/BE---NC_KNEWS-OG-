cf250d6112cd2b85d7555dfde587c52bdc04700b
Mohammed Azhar <a3h4r@hotmail.co.uk>
2019-02-17 12:58:29 +0000

    added heroku hosting and made changes to be able to host

--- listen.js
+++ listen.js
@@ -1,7 +1,11 @@
 const app = require('./app');
 
-const port = '9090';
+// const port = '9090';
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`listening on port ${port}`);
+// });
+
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
