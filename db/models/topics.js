51db51beadc05828496931b3931f9ae386e37007
Mohammed Azhar <a3h4r@hotmail.co.uk>
2019-02-26 13:03:28 +0000

    fixed total count for fetch articles by topic

--- db/models/topics.js
+++ db/models/topics.js
@@ -1,23 +1,41 @@
 const connection = require('../connection');
 
 exports.totalArticlesByTopic = topic => connection('articles')
-  .count('articles.article_id as total_count')
-  .where('topic', '=', topic)
-  .returning('*');
-
+    .count('articles.article_id as total_count')
+    .where('topic', '=', topic)
+    .returning('*')
+    .then(countArray => countArray[0].total_count);
 
 exports.fetchTopics = () => connection('topics').select('*');
 
-exports.addTopic = newTopic => connection('topics').insert(newTopic).returning('*');
+exports.addTopic = newTopic => connection('topics')
+    .insert(newTopic)
+    .returning('*');
 
-exports.fetchArticlesByTopic = (topic, limit = 10, sort_by = 'created_at', order = 'desc', p = 1) => connection('articles')
-  .select('articles.article_id', 'articles.username AS author', 'topic', 'title', 'articles.body', 'articles.votes', 'articles.created_at')
-  .leftJoin('comments', 'comments.article_id', '=', 'articles.article_id')
-  .groupBy('articles.article_id')
-  .count('comments.comment_id as comment_count')
-  .where('topic', '=', topic)
-  .orderBy(sort_by, order)
-  .limit(limit)
-  .offset((p - 1) * limit);
+exports.fetchArticlesByTopic = (
+  topic,
+  limit = 10,
+  sort_by = 'created_at',
+  order = 'desc',
+  p = 1,
+) => connection('articles')
+    .select(
+      'articles.article_id',
+      'articles.username AS author',
+      'topic',
+      'title',
+      'articles.body',
+      'articles.votes',
+      'articles.created_at',
+    )
+    .leftJoin('comments', 'comments.article_id', '=', 'articles.article_id')
+    .groupBy('articles.article_id')
+    .count('comments.comment_id as comment_count')
+    .where('topic', '=', topic)
+    .orderBy(sort_by, order)
+    .limit(limit)
+    .offset((p - 1) * limit);
 
-exports.addArticle = newArticle => connection('articles').insert(newArticle).returning('*');
+exports.addArticle = newArticle => connection('articles')
+    .insert(newArticle)
+    .returning('*');
