af05bd965d0f62fe66868439c5ae72993ab2c27d
Mohammed Azhar <a3h4r@hotmail.co.uk>
2019-02-28 01:17:48 +0000

    added new error for when article_id is out of range

--- errors/index.js
+++ errors/index.js
@@ -1,10 +1,12 @@
-
 exports.handle422 = (err, req, res, next) => {
   const codes = {
     23505: `duplicate key value violates unique constraint ----> ${err.detail}`,
   };
-  if (codes[err.code]) res.status(422).send({ message: `Error Code: ${res.statusCode} - ${codes[err.code]}` });
-  else next(err);
+  if (codes[err.code]) {
+    res
+      .status(422)
+      .send({ message: `Error Code: ${res.statusCode} - ${codes[err.code]}` });
+  } else next(err);
 };
 
 exports.handle400 = (err, req, res, next) => {
@@ -13,16 +15,29 @@ exports.handle400 = (err, req, res, next) => {
     23503: 'username does not exist',
     '22P02': 'invalid input syntax for type integer',
   };
-  if (codes[err.code]) res.status(400).send({ message: `Error Code: ${res.statusCode} - ${codes[err.code]}` });
-  else if (err.status === 400) {
-    res.status(400).send({ message: `Error Code: ${res.statusCode} - ${err.message}` });
+  if (codes[err.code]) {
+    res
+      .status(400)
+      .send({ message: `Error Code: ${res.statusCode} - ${codes[err.code]}` });
+  } else if (err.status === 400) {
+    res
+      .status(400)
+      .send({ message: `Error Code: ${res.statusCode} - ${err.message}` });
   } else next(err);
 };
 
-
 exports.handle404 = (err, req, res, next) => {
-  if (err.status === 404) {
-    res.status(404).send({ message: `Error Code: ${res.statusCode} - ${err.message}` });
+  const codes = {
+    22003: 'Article Not Found',
+  };
+  if (codes[err.code]) {
+    res
+      .status(404)
+      .send({ message: `Error Code: ${res.statusCode} - ${codes[err.code]}` });
+  } else if (err.status === 404) {
+    res
+      .status(404)
+      .send({ message: `Error Code: ${res.statusCode} - ${err.message}` });
   } else {
     next(err);
   }
@@ -33,5 +48,8 @@ exports.handle405 = (req, res) => {
 };
 
 exports.handle500 = (err, req, res, next) => {
-  res.status(500).send({ message: `${res.statusCode} - internal server error` });
+  console.log(err);
+  res
+    .status(500)
+    .send({ message: `${res.statusCode} - internal server error` });
 };
