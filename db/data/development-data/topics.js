991e66ba0a1116932d77b13195eda043ceef0505
Mohammed Azhar <a3h4r@hotmail.co.uk>
2019-03-13 05:30:28 +0000

    added image links for each topic

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -1,5 +1,17 @@
 module.exports = [
-  { description: 'Code is love, code is life', slug: 'coding' },
-  { description: 'FOOTIE!', slug: 'football' },
-  { description: 'Hey good looking, what you got cooking?', slug: 'cooking' },
+  {
+    description: 'Code is love, code is life',
+    slug: 'coding',
+    image: 'https://i.imgur.com/p2OaJR8.jpg',
+  },
+  {
+    description: 'FOOTIE!',
+    slug: 'football',
+    image: 'https://i.imgur.com/Q3JQEiq.jpg',
+  },
+  {
+    description: 'Hey good looking, what you got cooking?',
+    slug: 'cooking',
+    image: 'https://i.imgur.com/Tj7WrT9.jpg',
+  },
 ];
